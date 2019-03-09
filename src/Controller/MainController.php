1398aa346d91aaa919613d0f61bfd685670a3a4f
Valeriu <buzila@prescreen.io>
2019-03-09 13:06:41 +0200

    wip main controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,16 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route(host="{domain}", defaults={"domain" = "%domain%"}, requirements={"domain" = "%domain%"})
+ */
 class MainController extends AbstractController
 {
     /**
-     * @Route(
-     *     "/",
-     *     name="homepage",
-     *     host="{domain}",
-     *     defaults={"domain" = "%domain%"},
-     *     requirements={"domain" = "%domain%"}
-     * )
+     * @Route("/", name="homepage")
      */
     public function homepage(): JsonResponse
     {
