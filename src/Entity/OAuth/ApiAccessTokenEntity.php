3c005e3db462be1e5b12287c0188c346160ac719
Valeriu <buzila@prescreen.io>
2019-03-13 22:06:42 +0200

    wip oauth

---
+++ src/Entity/OAuth/ApiAccessTokenEntity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity\OAuth;
+
+use App\Entity\BaseEntityTrait;
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\AccountUser;
+use App\Entity\BaseAccountUserEntityTrait;
+use League\OAuth2\Server\AuthorizationServer;
+use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OAuth\ApiAccessTokenRepository")
+ * @ORM\HasLifecycleCallbacks
+ */
+class ApiAccessTokenEntity implements AccessTokenEntityInterface
+{
+    use AccessTokenTrait, TokenEntityTrait, EntityTrait;
+    use BaseEntityTrait;
+    use BaseAccountUserEntityTrait;
+
+    /**
+     * @var AccountUser
+     * @ORM\OneToOne(targetEntity="App\Entity\AccountUser")
+     * @ORM\JoinColumn(fieldName="account_user_id", nullable=false, referencedColumnName="id")
+     */
+    protected $accountUser;
+
+}
